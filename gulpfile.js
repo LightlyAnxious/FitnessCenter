752c50ac562638efdd003ffe4205605f68bb511f
LightlyAnxious <daniljukebox@gmail.com>
2020-02-20 14:27:24 +0300

    Добавлены оптимизированные изображения

--- gulpfile.js
+++ gulpfile.js
@@ -25,13 +25,13 @@ gulp.task("css", function () {
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -99,5 +99,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "images", "copy", "css", "html"));
 gulp.task("start", gulp.series("css", "server"));
